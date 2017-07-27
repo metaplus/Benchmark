683e9646f5729caf338d675fcbb9bb0401504c13
Jingxing Xu <jx.xu@outlook.com>
2017-07-28 00:05:09 +0800

    clion commit

--- IO/main.cpp
+++ IO/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+#include <laser/basis.h>
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    fstream fs;
+    thread t{[]{
+        cout<<"laji"<<en;
+    }};
+    t.join();
     return 0;
 }
